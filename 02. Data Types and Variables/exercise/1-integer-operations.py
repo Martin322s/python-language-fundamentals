b35790c55845dccf81999596166d79c8f398a994
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-01-06 15:52:45 +0200

    1-integers-operations

---
+++ 02. Data Types and Variables/exercise/1-integer-operations.py
@@ -0,0 +1,10 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+fourth_number = int(input())
+
+first_result = first_number + second_number
+second_result = int(first_result / third_number) 
+final_result = int(second_result * fourth_number) 
+
+print(final_result)
