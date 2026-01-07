b2f1fbee7f0c9a722d50ce928adfbf7164f96d89
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-01-07 15:54:41 +0200

    7-water-overflow

---
+++ 02. Data Types and Variables/exercise/7-water-overflow.py
@@ -0,0 +1,12 @@
+capacity = 255
+n = int(input())
+tank = 0
+
+for _ in range(n):
+    liters = int(input())
+    if tank + liters > capacity:
+        print("Insufficient capacity!")
+    else:
+        tank += liters
+
+print(tank)
