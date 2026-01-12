0fbd36dbe0acafb9cabe47c071f2f10faa0d9b9d
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-01-12 15:04:26 +0200

    7-easter-gifts

---
+++ 03. Lists Basics/exercise/7-easter-gifts.py
@@ -0,0 +1,29 @@
+gifts = input().split()
+
+while True:
+    command = input()
+    if command == "No Money":
+        break
+
+    parts = command.split()
+    action = parts[0]
+
+    if action == "OutOfStock":
+        gift = parts[1]
+        for i in range(len(gifts)):
+            if gifts[i] == gift:
+                gifts[i] = "None"
+
+    elif action == "Required":
+        gift = parts[1]
+        index = int(parts[2])
+        if 0 <= index < len(gifts):
+            gifts[index] = gift
+
+    elif action == "JustInCase":
+        gift = parts[1]
+        gifts[-1] = gift
+
+result = [g for g in gifts if g != "None"]
+
+print(" ".join(result))
