aaa6e93652c56cccd7dbdb8eb01905cf72f581f1
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-01-23 01:55:08 +0200

    2-next-version-py

---
+++ 05. Lists Advanced/exercise/2-next-version.py
@@ -0,0 +1,11 @@
+version = input().split(".")
+version = [int(x) for x in version]
+
+version[2] += 1
+
+for i in range(2, 0, -1):
+    if version[i] > 9:
+        version[i] = 0
+        version[i - 1] += 1
+
+print(".".join(str(x) for x in version))
