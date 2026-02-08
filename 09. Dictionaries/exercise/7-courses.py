dbd6cc2b75ae62cfdba170b9cb5cac74280621ea
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2026-02-08 23:56:28 +0200

    courses py file

---
+++ 09. Dictionaries/exercise/7-courses.py
@@ -0,0 +1,17 @@
+courses = {}
+
+while True:
+    line = input()
+    if line == "end":
+        break
+
+    course, student = line.split(" : ")
+
+    if course not in courses:
+        courses[course] = []
+    courses[course].append(student)
+
+for course, students in courses.items():
+    print(f"{course}: {len(students)}")
+    for st in students:
+        print(f"-- {st}")
